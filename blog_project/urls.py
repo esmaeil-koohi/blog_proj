79bd8f9580b65a9c3584d6ca6d360b2e0b9a8371
esmaeil-koohi <esmaeil76koohi@gmail.com>
2024-01-09 00:38:41 +0330

    completed article

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path, include
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('', include('home_app.urls')),
     path('test/', include('account_app.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home_app/views.py
+++ home_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from blog.models import Article
 
 
 def home(request):
-    return render(request, "home_app/index.html", {})
+    articles = Article.objects.all()
+    print(articles)
+    return render(request, 'home_app/index.html', {'article': articles})
